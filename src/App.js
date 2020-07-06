18da1ea3718b5aec74c24cc184bdf95b7fd2a02e
Woo Cashpirowski <cashpirowski@gmail.com>
2020-07-06 18:03:03 +0200

    app component structure

--- src/App.js
+++ src/App.js
@@ -6,12 +6,53 @@ import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
 
 export class App extends Component {
+  state = {
+    items: [
+      { id: 1, title: "wake up!" },
+      { id: 2, title: "rule the world" },
+    ],
+    id: uuidv4(),
+    item: "",
+    editItem: false,
+  };
+
+  handleChange = (e) => {
+    console.log("handle change");
+  };
+
+  handleSubmit = (e) => {
+    console.log("handle submit");
+  };
+  clearList = (e) => {
+    console.log("clear list");
+  };
+  handleDelete = (id) => {
+    console.log(`delete ${id}`);
+  };
+  handleEdit = (id) => {
+    console.log(`edit ${id}`);
+  };
+
   render() {
     return (
-      <div>
-        <h1>Hello from app</h1>
-        <TodoInput />
-        <TodoList />
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto col-md-8 mt-4">
+            <h3 className="text-capitalize text-center">Todo input</h3>
+            <TodoInput
+              item={this.state.item}
+              handleChange={this.handleChange}
+              handleSubmit={this.handleSubmit}
+              editItem={this.state.editItem}
+            />
+            <TodoList
+              items={this.state.items}
+              clearList={this.clearList}
+              handleDelete={this.handleDelete}
+              handleEdit={this.handleEdit}
+            />
+          </div>
+        </div>
       </div>
     );
   }
