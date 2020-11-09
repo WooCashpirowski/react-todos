7bd24fd5311797dcabe2aead057ffe9fab8d841f
WooCashpirowski <cashpirowski@gmail.com>
2020-11-09 09:04:18 +0100

    heading removal

--- src/App.js
+++ src/App.js
@@ -60,7 +60,6 @@ export class App extends Component {
       <div className="container">
         <div className="row">
           <div className="col-10 mx-auto col-md-8 mt-4">
-            <h3 className="text-capitalize text-center">Todo input</h3>
             <TodoInput
               item={this.state.item}
               handleChange={this.handleChange}
